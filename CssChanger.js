48da4cb000ca56928ff20072a855395e506e39fe
eboody <eboodnero@gmail.com>
2020-06-06 19:32:30 -0700

    cleaned up CssChanger.js

--- CssChanger.js
+++ CssChanger.js
@@ -2,20 +2,18 @@
 // @name         CSS Changer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  change css stylesheets for roam with F9 and F10
+// @author       @eboodnero
 // @match        http://*/*
 // @grant        none
 // ==/UserScript==
 const sheet = new CSSStyleSheet();
 document.adoptedStyleSheets = [sheet];
 document.onkeyup = function(e) {
-  if (/*e.ctrlKey && */e.which == 120) {
-      // alert("CTRL + F7 was pressed");
+  if (/*F9*/e.which == 120) {
       sheet.replaceSync("https://raw.githubusercontent.com/eboody/Roam-Scripts/master/stylesheet1.css");
   }
-  else if (/*e.ctrlKey && */e.which == 121) {
-      // alert("CTRL + F7 was pressed");
+  else if (/*F10 */e.which == 121) {
       sheet.replaceSync("https://raw.githubusercontent.com/eboody/Roam-Scripts/master/stylesheet2.css");
   }
 };
