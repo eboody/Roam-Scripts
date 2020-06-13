17744908f1a6b9b20bbae3074d7e0b4d8496e668
eboody <eboodnero@gmail.com>
2020-06-13 05:15:43 -0700

    added setTimeouts before normalize and highlight calls so that bullets would update when pressing Enter

--- ScopeCue.js
+++ ScopeCue.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         ScopeCue
+// @namespace    http://tampermonkey.net/
 // @version      2.5
-// @description  Highlights which scope you're in
+// @description  makes the line to the sibling block orange
 // @author       @eboodnero
+// @match        https://www.w3schools.com/tags/ref_colornames.asp
 // @grant        none
 // ==/UserScript==
 
@@ -15,6 +17,7 @@ function addListenerToBlocks(){
     //highlight elements on mouse up
     Array.prototype.map.call(document.querySelectorAll(".flex-v-box.roam-block-container.block-bullet-view"), e => e.onmouseup = function(e){
         highlight();
+        setTimeout(highlight, 50)
     })
 }
 //highlight the text area bullet, the left border of children of the sibling and the bullet of the sibling
@@ -50,8 +53,6 @@ function highlight (){
         }
     }
 }
-
-//un-highlight stuff
 function normalize(){
     //make all borders normal color
     Array.prototype.map.call(document.querySelectorAll(".block-border-left"), e => e.style.borderColor = oldColor)
@@ -61,7 +62,7 @@ function normalize(){
     document.querySelector("textarea").parentElement.parentElement.querySelector(".simple-bullet-inner").style.backgroundColor = oldBulletColor
 }
 
-//do this to initialize variable and listeners
+
 function initialize(){
     //get the initial colors of inner, outer bullets and borders
     oldColor = window.getComputedStyle(document.querySelector('.block-border-left')).borderColor
@@ -72,12 +73,14 @@ function initialize(){
     document.onkeydown = function(e) {
         if( e.which == 8 || e.which == 9 || (e.shiftKey && e.which == 9) || e.which == 13 || e.which == 38 || e.which == 40 || (e.altKey && e.shiftKey && e.which == 37)|| (e.altKey && e.shiftKey && e.which == 38)|| (e.altKey && e.shiftKey && e.which == 39)|| (e.altKey && e.shiftKey && e.which == 40) || (e.which == 27)){
             normalize();
+            setTimeout(normalize, 50)
         }
     };
     //if I press any of these keys highlight the colors of the elements mentioned above
     document.onkeyup = function(e) {
         if ( e.which == 8 || e.which == 9 || (e.shiftKey && e.which == 9) || e.which == 13 || e.which == 38 || e.which == 40 || (e.altKey && e.shiftKey && e.which == 37)|| (e.altKey && e.shiftKey && e.which == 38)|| (e.altKey && e.shiftKey && e.which == 39)|| (e.altKey && e.shiftKey && e.which == 40) ){
             highlight();
+            setTimeout(highlight, 50)
         }
     };
     //refresh the listeners on mouseup
@@ -88,6 +91,7 @@ function initialize(){
     //normalize and refresh listeners on mousedown
     document.onmousedown = function(){
         normalize();
+        setTimeout(normalize, 50)
     }
 }
 //don't initialize until the h1 element exists
